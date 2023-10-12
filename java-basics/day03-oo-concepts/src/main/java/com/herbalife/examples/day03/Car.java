f0e356e983541149a3d19ddd1eb3a3e5931594f6
Prabhu Sunderaraman <prabhu.bits@gmail.com>
2023-10-12 08:29:15 +0530

    Day03

---
+++ java-basics/day03-oo-concepts/src/main/java/com/herbalife/examples/day03/Car.java
@@ -0,0 +1,22 @@
+package com.herbalife.examples.day03;
+
+//com -> commercial
+//org -> open source
+//java. jakarta. -> core packages
+
+import com.herbalife.examples.wednesday.Engine;
+//import com.herbalife.examples.wednesday.Wheel;
+
+//packages are physical grouping of folders
+//public classes have to be stored in files with the same name as the class
+//there can be ONLY ONE public class in a .java file
+public class Car {
+    public static void main(String[] args) {
+        Engine engine = new Engine();
+        //engine.power = 1200;
+        //Wheel wheel = new Wheel();
+    }
+}
+//public class Engine {
+//
+//}
---
+++ java-basics/day03-oo-concepts/src/main/java/com/herbalife/examples/wednesday/Engine.java
@@ -0,0 +1,13 @@
+package com.herbalife.examples.wednesday;
+
+public class Engine {
+    public Engine() {}
+    int power; //default is package-friendly
+}
+
+class Wheel {} //default access specifier is package-friendly
+
+class Workshop {
+    String title;
+    String address;
+}
