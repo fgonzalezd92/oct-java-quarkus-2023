282f0e83e8f35f8e559db61f00965363d55245d0
Prabhu Sunderaraman <prabhu.bits@gmail.com>
2023-10-17 09:14:20 +0530

    Day05

--- java-basics/day05-functional-interfaces/src/main/java/com/herbalife/labs/lab07/ValidationConstants.java
+++ java-basics/day05-functional-interfaces/src/main/java/com/herbalife/labs/lab07/ValidationConstants.java
@@ -7,11 +7,18 @@ public interface ValidationConstants {
     Predicate<String> isNotNull = value -> value != null;
     Predicate<String> isNotBlank = value -> !value.isBlank();
     Predicate<String> isNotEmpty = value -> !value.isEmpty();
+    //Job security driven development;
     BiFunction<Integer, Integer, Predicate<String>> lengthValidator = (min, max) -> value -> value.length() >= min && value.length() <= max;
     Predicate<String> hasUpperCase = value -> value.matches(".*[A-Z].*");
     Predicate<String> hasDigit = value -> value.matches(".*\\d+.*");
 
 //    Predicate<String> isUsernameLengthValid = value -> value.length() >= 6 && value.length() <= 12;
 //    Predicate<String> isPasswordLengthValid = value -> value.length() >= 8 && value.length() <= 14;
+
+    //Collective ownership
+    BiFunction<Integer, Integer, Predicate<String>> lengthValidator2 = (Integer min, Integer max) -> {
+        Predicate<String> lengthPredicate = (String value) -> value.length() >= min && value.length() <= max;
+        return lengthPredicate;
+    };
 
 }
