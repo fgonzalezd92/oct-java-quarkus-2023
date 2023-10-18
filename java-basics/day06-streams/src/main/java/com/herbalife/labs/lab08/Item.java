2b983858e2778787a1915d81e0983a4f05a72d54
Prabhu Sunderaraman <prabhu.bits@gmail.com>
2023-10-18 08:15:20 +0530

    Day06

---
+++ java-basics/day06-streams/src/main/java/com/herbalife/labs/lab08/Item.java
@@ -0,0 +1,28 @@
+package com.herbalife.labs.lab08;
+
+
+public class Item {
+    private String name;
+    private double price;
+
+    public Item(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
---
+++ java-basics/day06-streams/src/main/java/com/herbalife/labs/lab08/Lab08Main.java
@@ -0,0 +1,37 @@
+package com.herbalife.labs.lab08;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Lab08Main {
+    public static void main(String[] args) {
+        List<Item> items = Arrays.asList(new Item("Nike", 20.45),
+                new Item("Adidas", 31.45),
+                new Item("Reebok", 29.25),
+                new Item("Puma", 25.15),
+                new Item("Fila", 15.15));
+
+        //Print the name and price of the costliest shoe
+        Item costliestItem = items
+                .stream()
+                .max((item1, item2) -> Double.compare(item1.getPrice(), item2.getPrice()))
+                .get();
+        System.out.println(costliestItem);
+
+        //Print the name and price of the costliest shoe
+        Item cheapestItem = items
+                .stream()
+                .min((item1, item2) -> Double.compare(item1.getPrice(), item2.getPrice()))
+                .get();
+        System.out.println(cheapestItem);
+
+        //Concatenate all the item names using comma and print the value
+        String names = items
+                .stream()
+                //.map(item -> item.getName())
+                .map(Item::getName)
+                .collect(Collectors.joining(","));
+        System.out.println(names);
+    }
+}
