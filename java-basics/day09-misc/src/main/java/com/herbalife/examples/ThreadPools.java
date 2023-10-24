978f30dd3a5dde6bb68045798b9439fe8dc2aae3
Prabhu Sunderaraman <prabhu.bits@gmail.com>
2023-10-24 09:24:29 +0530

    Day09

--- java-basics/day09-misc/src/main/java/com/herbalife/examples/ThreadPools.java
+++ java-basics/day09-misc/src/main/java/com/herbalife/examples/ThreadPools.java
@@ -1,7 +1,45 @@
 package com.herbalife.examples;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ForkJoinPool;
+
 public class ThreadPools {
     public static void main(String[] args) {
+        //Create a thread pool, by specifying the number of threads
+        //pick up a thread from the pool
+        //execute a task using that thread
 
+        //ExecutorService executorService = Executors.newFixedThreadPool(2);
+        //ExecutorService executorService = Executors.newCachedThreadPool();
+        ForkJoinPool executorService = ForkJoinPool.commonPool(); //The threads terminate when main terminates
+
+        Runnable codingTask = () -> {
+            while(true) {
+                System.out.println("Coding and committing changes... on " + Thread.currentThread().getName());
+                MyThreadUtil.sleep(3);
+            }
+        };
+        executorService.submit(codingTask);
+
+        Runnable browsingTask = () -> {
+            int count = 0;
+            while(count < 5) {
+                System.out.println("Browsing... on " + Thread.currentThread().getName());
+                MyThreadUtil.sleep(4);
+                count++;
+            }
+        };
+        executorService.submit(browsingTask);
+
+        Runnable eatingTask = () -> {
+            while(true) {
+                System.out.println("Eating... on " + Thread.currentThread().getName());
+                MyThreadUtil.sleep(5);
+            }
+        };
+        executorService.submit(eatingTask);
+
+        MyThreadUtil.sleep(100); //Required for ForkJoinPool
     }
 }
