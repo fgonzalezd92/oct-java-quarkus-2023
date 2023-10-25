36f9022321cd9f88bf82b15e5426907aae66c703
Prabhu Sunderaraman <prabhu.bits@gmail.com>
2023-10-25 08:30:59 +0530

    Day10

--- java-basics/day10-more-concurrency/src/main/java/com/herbalife/examples/ForEach.java
+++ java-basics/day10-more-concurrency/src/main/java/com/herbalife/examples/ForEach.java
@@ -10,16 +10,20 @@ public class ForEach {
     public static void main(String[] args) {
         List<Integer> numbers = List.of(11, 45, 126, 23, 4, 99, 34, 12, 23, 45, 67, 89);
 
-        List<Integer> resultList = new ArrayList<>(); //not thread safe
-        numbers
-                .parallelStream()
-                .map(e -> e % e)
-                //.collect(Collectors.toList())
-                .forEach(e -> {
-                    resultList.add(e); //DANGER; Ticking timebomb
-                });
+        for (int i = 0; i < 10; i++) {
 
-        System.out.println("Numbers: " + numbers.size());
-        System.out.println("Result " + resultList.size()); //at times this will be less than 12
+            List<Integer> resultList = new ArrayList<>(); //not thread safe
+            numbers
+                    .parallelStream()
+                    .map(e -> e % e)
+                    //.collect(Collectors.toList());
+                    .forEach(e -> {
+                        resultList.add(e); //DANGER; Ticking timebomb
+                    });
+
+            System.out.println("Numbers: " + numbers.size());
+            System.out.println("Result " + resultList.size()); //at times this will be less than 12
+        }
+
     }
 }
